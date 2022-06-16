7a7636b1656d1abdd7cf13eccfb29793c3724e41
leidy <leidy1234711@gmail.com>
2022-06-16 14:33:00 -0500

    limpieza de vistas

--- weather_app/weather/urls.py
+++ weather_app/weather/urls.py
@@ -5,11 +5,4 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name = 'home'),
     path('delete/<city_name>/', views.delete_city, name='delete_city'),
-    path('prueba', views.prueba, name='prueba'),
-    path('index-prueba',views.index_prueba, name='index_prueba'),
-    path('pagina-prueba', views.pagina2, name='pagina_prueba'),
-    path('pagina-prueba3', views.pagina3, name='pagina_prueba3'),
-    path('pagina-prueba4', views.pagina4, name='pagina_prueba4'),
-    path('pagina-prueba5', views.pagina5, name='pagina_prueba5'),
-    path('sample-view', views.sample_view, name='sample_view')
 ]
