0e18c4a6de6ada78d3db6bed2b2ef7369b66ff44
leidy <leidy1234711@gmail.com>
2022-06-17 15:48:22 -0500

    vistas

--- weather_app/weather/urls.py
+++ weather_app/weather/urls.py
@@ -5,11 +5,12 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name = 'home'),
     path('delete/<city_name>/', views.delete_city, name='delete_city'),
-    path('prueba', views.prueba, name='prueba'),
-    path('index-prueba',views.index_prueba, name='index_prueba'),
-    path('pagina-prueba', views.pagina2, name='pagina_prueba'),
-    path('pagina-prueba3', views.pagina3, name='pagina_prueba3'),
-    path('pagina-prueba4', views.pagina4, name='pagina_prueba4'),
-    path('pagina-prueba5', views.pagina5, name='pagina_prueba5'),
-    path('sample-view', views.sample_view, name='sample_view')
+    path('prueba-view', views.Prueba.as_view(), name='prueba_view'),
+    path('index-view',views.IndexPrueba.as_view(), name='index_view'),
+    path('pagina2-view', views.VistaDos.as_view(), name='pagina2_view'),
+    path('pagina3-view', views.VistaTres.as_view(), name='pagina3_view'),
+    path('pagina4-view', views.VistaCuatro.as_view(), name='pagina4_view'),
+    path('pagina5-view', views.VistaCinco.as_view(), name='pagina5_view'),
+    path('sample-view', views.SampleView.as_view(), name='sample_view'),
+    path('class-view', views.VistaPrueba.as_view(), name='class_view')
 ]
